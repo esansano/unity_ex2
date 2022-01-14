4acd2d6056144ea5a788e8e95c54d0a6bcb11bf2
Emilio <esansano@uji.es>
2022-01-14 13:44:45 +0100

    cosas

--- Assets/Scripts/Proyectil.cs
+++ Assets/Scripts/Proyectil.cs
@@ -9,13 +9,18 @@ public class Proyectil : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+    }
+
+    public void Fire(float verticalForce) {
         proyectilRB = GetComponent<Rigidbody>();
-        proyectilRB.AddForce(new Vector3(0, 2.5f, -6f), ForceMode.Impulse);
+        proyectilRB.AddForce(new Vector3(0, verticalForce, -3f), ForceMode.Impulse);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.y < -5) {
+            Destroy(gameObject);
+        }
     }
 }
